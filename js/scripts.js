4b92d3c67dfa8bfa02ec30cb1911df167c141416
Raveli <miguelraveli123@gmail.com>
2024-11-27 21:47:24 -0300

    Colocando minha versão junto com a ateração da main

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,25 @@
 function menuOnClick() {
-    document.getElementById("menu-bar").classList.toggle("change");
-    document.getElementById("nav").classList.toggle("change");
-    document.getElementById("menu-bg").classList.toggle("change-bg");
+  document.getElementById("menu-bar").classList.toggle("change");
+  document.getElementById("nav").classList.toggle("change");
+  document.getElementById("menu-bg").classList.toggle("change-bg");
+}
+
+let dropDownOpen = true;
+function animationDropDown() {
+  const container_secao1 = document.getElementById("cont_secao1");
+  const secao1_botao = document.querySelector(".secao1-botao");
+  const currentHeight = parseInt(
+    window.getComputedStyle(container_secao1).height
+  );
+  if (dropDownOpen) {
+    container_secao1.style.height = currentHeight + 200 + "px"; // Aumenta 200px toda vez que clicar
+    dropDownOpen = false;
+    secao1_botao.classList.add("removeBorderRadiusSecao1Botao");
+    secao1_botao.style.backgroundColor = "#8fcce6";
+  } else {
+    container_secao1.style.height = currentHeight - 200 + "px"; // Diminui 200px toda vez que clicar
+    dropDownOpen = true;
+    secao1_botao.classList.remove("removeBorderRadiusSecao1Botao");
+    secao1_botao.style.backgroundColor = "#6ec1e4";
   }
+}
