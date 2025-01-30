4ff903d5ca8b19af53ee9c1754cb7433f61384b6
Pedrohlatorre <pedrohlatorre@icloud.com>
2025-01-30 14:43:55 -0300

    Início do código js - Menu-hamburguer

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,13 @@
-function menuOnClick() {
-    document.getElementById("menu-bar").classList.toggle("change");
-    document.getElementById("nav").classList.toggle("change");
-    document.getElementById("menu-bg").classList.toggle("change-bg");
-  }
+// function menuOnClick() {
+//     document.getElementById("menu-bar").classList.toggle("change");
+//     document.getElementById("nav").classList.toggle("change");
+//     document.getElementById("menu-bg").classList.toggle("change-bg");
+//   }
+
+
+let btnMobile = document.getElementById("btn-mobile")
+let menu = document.getElementById("menu")
+
+btnMobile.addEventListener("click", function openMenu () {
+    menu.classList.toggle("visible")
+})
