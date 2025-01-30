f7fd11952fbbef03124839093af06adb670c83ba
Pedrohlatorre <pedrohlatorre@icloud.com>
2025-01-30 16:51:01 -0300

    finalização menu hamburguer, fale conosco ativo e botao whatsapp

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,32 @@
-// function menuOnClick() {
-//     document.getElementById("menu-bar").classList.toggle("change");
-//     document.getElementById("nav").classList.toggle("change");
-//     document.getElementById("menu-bg").classList.toggle("change-bg");
-//   }
-
-
+// Botão Menu Hamburguer para ativação na versão mobile para navegação das páginas
 let btnMobile = document.getElementById("btn-mobile")
 let menu = document.getElementById("menu")
 
-btnMobile.addEventListener("click", function openMenu () {
+btnMobile.addEventListener("click", function openMenu (event) {
+    event.preventDefault()
     menu.classList.toggle("visible")
+})
+
+
+
+// Botão Fale Conosco da index.html para contato com os vendedores
+let btnFaleConosco = document.getElementById("btn-faleconosco")
+let listaFaleConosco = document.getElementById("lista-faleconosco")
+
+btnFaleConosco.addEventListener("click", function (event) {
+    event.preventDefault()
+    listaFaleConosco.classList.toggle("visible")
+    btnFaleConosco.classList.toggle("secao1-botao-sembordaembaixo")
+})
+
+
+
+let btnWhatsapp = document.getElementById("btn-whatsapp")
+let cardWhatsapp = document.getElementById("card-whatsapp")
+let balaoWhatsapp = document.getElementById("balao-whatsapp")
+
+btnWhatsapp.addEventListener("click", function (event) {
+    event.preventDefault()
+    cardWhatsapp.classList.toggle("visible")
+    balaoWhatsapp.classList.toggle("invisible")
 })
